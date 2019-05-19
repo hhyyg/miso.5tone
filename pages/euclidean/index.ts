27b24102ce1679626ebdae50d8ec9380a962ffe0
hhyyg <hiroka.yago@gmail.com>
2019-05-19 16:51:53 +0900

    Add new page

---
+++ pages/euclidean/index.ts
@@ -0,0 +1,12 @@
+/// <reference path="../../types/p5/global.d.ts"/>
+/// <reference path="../../types/p5/index.d.ts"/>
+
+
+function setup() {
+    createCanvas(windowWidth, windowHeight);
+    stroke(0, 0, 0);
+}
+
+function draw() {
+    rect(10, 10, 100, 100);
+}
