cf0c5c16d01b4d7982e986fbdd1abfe432b675d2
hhyyg <hiroka.yago@gmail.com>
2019-05-20 21:38:15 +0900

    Draw dummy 'k'

--- pages/euclidean/index.js
+++ pages/euclidean/index.js
@@ -3,31 +3,48 @@
 var n = 8;
 var k = 3;
 var e = [1, 0, 0, 1, 0, 0, 1, 0];
+var angleStep = 360 / n;
 var radius = 300;
 var v1;
 function setup() {
     createCanvas(windowWidth, windowHeight);
     stroke(0, 0, 0);
+    fill(255, 255, 255, 0);
+    textSize(32);
     v1 = { x: windowWidth / 2, y: windowHeight / 2 };
     ;
 }
 function draw() {
     drawCircle();
     drawN(n);
+    drawK(e);
 }
 function drawCircle() {
     ellipse(v1.x, v1.y, radius * 2);
 }
 function drawN(n) {
-    var angleStep = 360 / n;
     beginShape();
-    for (var angle = 0; angle <= 360; angle += angleStep) {
+    var cursor = 0;
+    for (var angle = -90; angle < 270; angle += angleStep) {
         var vx = v1.x + cos(radians(angle)) * radius;
         var vy = v1.y + sin(radians(angle)) * radius;
         vertex(vx, vy);
+        ellipse(vx, vy, 20);
+        text(cursor.toString(), vx, vy);
+        cursor += 1;
     }
-    endShape();
+    endShape(CLOSE);
 }
-function drawK() {
-    // next
+function drawK(e) {
+    beginShape();
+    var cursor = 0;
+    for (var angle = -90; angle <= 270; angle += angleStep) {
+        if (e[cursor] === 1) {
+            var vx = v1.x + cos(radians(angle)) * radius;
+            var vy = v1.y + sin(radians(angle)) * radius;
+            vertex(vx, vy);
+        }
+        cursor += 1;
+    }
+    endShape(CLOSE);
 }
--- pages/euclidean/index.ts
+++ pages/euclidean/index.ts
@@ -9,12 +9,16 @@ type P = {
 const n = 8;
 const k = 3;
 const e = [1, 0, 0, 1, 0, 0, 1, 0];
+
+const angleStep = 360 / n;
 const radius = 300;
 let v1:P;
 
 function setup() {
     createCanvas(windowWidth, windowHeight);
     stroke(0, 0, 0);
+    fill(255, 255, 255, 0);
+    textSize(32);
 
     v1 = { x: windowWidth / 2, y: windowHeight / 2 };;
 }
@@ -22,6 +26,7 @@ function setup() {
 function draw() {
     drawCircle();
     drawN(n);
+    drawK(e);
 }
 
 function drawCircle() {
@@ -29,19 +34,34 @@ function drawCircle() {
 }
 
 function drawN(n: number) {
-    const angleStep = 360 / n;
-
     beginShape();
 
-    for(let angle = 0; angle <= 360; angle += angleStep) {
+    let cursor = 0;
+    for(let angle = -90; angle < 270; angle += angleStep) {
         const vx = v1.x + cos(radians(angle)) * radius;
         const vy = v1.y + sin(radians(angle)) * radius;
         vertex(vx, vy);
+        ellipse(vx, vy, 20);
+
+        text(cursor.toString(), vx, vy);
+        cursor += 1;
     }
 
-    endShape();
+    endShape(CLOSE);
 }
 
-function drawK() {
-    // next
+function drawK(e: number[]) {
+    beginShape();
+    let cursor = 0;
+
+    for(let angle = -90; angle <= 270; angle += angleStep) {
+
+        if (e[cursor] === 1) {
+            const vx = v1.x + cos(radians(angle)) * radius;
+            const vy = v1.y + sin(radians(angle)) * radius;
+            vertex(vx, vy);
+        }
+        cursor += 1;
+    }
+    endShape(CLOSE);
 }
